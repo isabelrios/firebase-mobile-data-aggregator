467c748ce61757a262083ec513c88f9eaaf24724
Aaron Train <aaron.train@gmail.com>
2021-11-09 10:04:23 -0500

    Update firebase.py and client.py

--- client.py
+++ client.py
@@ -50,10 +50,15 @@ def main():
     args = parse_args(sys.argv[1:])
 
     FirebaseHelperClient = FirebaseHelper(args.project, args.filter_by_name)
-    #FirebaseHelperClient.get_test_case_details(ExecutionOutcome.FAILURE.value)
+    #FirebaseHelperClient.get_test_case_details(ExecutionOutcome.SUCCESS.value)
     FirebaseHelperClient.print_test_results_by_execution_summary(
         execution_outcome_summary=ExecutionOutcome.SUCCESS.value
     )
+    # FirebaseHelperClient.print_test_results_by_execution_summary(
+    #     execution_outcome_summary=ExecutionOutcome.SUCCESS.value
+    # )
+    #FirebaseHelperClient.display_execution_timestamp(ExecutionOutcome.SUCCESS.value)
+
 
 
 if __name__ == '__main__':
--- firebase.py
+++ firebase.py
@@ -4,7 +4,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-"""A script to pull recent test results summary"""
+"""A small Firebase CloudToolsResults API client"""
 
 from __future__ import absolute_import
 
@@ -41,12 +41,13 @@ class Firebase:
         ).execute()
         return histories
 
-    def get_executions(self, history_id: str) -> dict:
+    def get_executions(self, history_id: str, page_token: str = None) -> dict:
         """Get a list of (default: 25) executions for a given project """
         executions = self.projects_client.projects().histories().executions().list(
             projectId=self.projectId,
             historyId=history_id,
-            pageSize=int(40)
+            pageSize=int(25),
+            pageToken=page_token
         ).execute()
         return executions
 
@@ -72,14 +73,14 @@ class Firebase:
         ).execute()
         return test_cases
 
-    def get_environment(self, history_id: str, execution_id: int) -> dict:
-        """Get the environment for a given execution"""
-        environment = self.projects_client.projects().histories().executions().environments().list(
+    def get_environments(self, history_id: str, execution_id: int) -> dict:
+        """Get the environments for a given execution"""
+        environments = self.projects_client.projects().histories().executions().environments().list(
             projectId=self.projectId,
             historyId=history_id,
             executionId=execution_id,
         ).execute()
-        return environment
+        return environments
 
 
 class FirebaseHelper:
@@ -90,9 +91,9 @@ class FirebaseHelper:
         """Get a list of all test histories"""
         return self.firebase.get_histories()
 
-    def get_executions(self, history_id: str) -> dict:
+    def get_executions(self, history_id: str, page_token: str) -> dict:
         """Get a list of all test executions"""
-        return self.firebase.get_executions(history_id)
+        return self.firebase.get_executions(history_id, page_token)
 
     def get_steps(self, history_id: str, execution_id: int, page_size: int) -> dict:
         """Get a list of all test steps"""
@@ -102,9 +103,9 @@ class FirebaseHelper:
         """Get a list of test cases attached to a Step"""
         return self.firebase.get_test_cases(history_id, execution_id, step_id, page_size)
 
-    def get_environment(self, history_id: str, execution_id: int) -> dict:
-        """Get the environment for a given execution"""
-        return self.firebase.get_environment(history_id, execution_id)
+    def get_environments(self, history_id: str, execution_id: int) -> dict:
+        """Get the environments for a given execution"""
+        return self.firebase.get_environments(history_id, execution_id)
 
     def get_test_case_results(self, history_id: str, execution_id: int, step_id: str, page_size: int) -> dict:
         """Get a list of test case results attached to a Step"""
@@ -112,82 +113,110 @@ class FirebaseHelper:
         return test_case_results
 
     def get_test_case_details(self, execution_outcome_summary: str) -> dict:
-        """Get test cases with provided execution outcome summary"""
+        """Get test case details with provided execution outcome summary"""
+        history = next(iter([x for y in self.get_histories().values() for x in y]))
+        executions = self.get_executions(
+            history_id=history['historyId'],
+            page_token=None
+        )
         results = []
-        histories = self.get_histories()
-        for history in histories.values():
-            for id in history:
-                executions = self.get_executions(
-                    history_id=id['historyId']
-                )
-                for execution in executions['executions']:
-                    """Filter on complete executions"""
-                    if 'complete' in execution.values():
-                        if execution['outcome']['summary'] == execution_outcome_summary:
-                            steps = self.get_steps(
-                                history_id=id['historyId'],
-                                execution_id=int(execution['executionId']),
-                                page_size=int(200)
-                            )
-                            for k, v in steps.items():
-                                if k == 'steps':
-                                    for step in v:
-                                        if step['outcome']['summary'] == 'failure':
-                                            cases = self.get_test_cases(
-                                                history_id=id['historyId'],
-                                                execution_id=int(execution['executionId']),
-                                                step_id=step['stepId'],
-                                                page_size=int(200)
-                                            )
-                                            # TODO: Decide on which data to capture
-                                            results.append( {
-                                                'testCase': [case['testCaseReference'] for case in cases['testCases']],
-                                                'result': [case['status'] for case in cases['testCases']],
-                                                'matrix': execution['testExecutionMatrixId']
-                                            })
-        print(results)
-   
+
+        for execution in executions['executions']:
+            """Filter on complete executions"""
+            if 'complete' in execution.values():
+                if execution['outcome']['summary'] == execution_outcome_summary:
+                    steps = self.get_steps(
+                        history_id=history['historyId'],
+                        execution_id=int(execution['executionId']),
+                        page_size=int(200)
+                    )
+                    for k, v in steps.items():
+                        if k == 'steps':
+                            for step in v:
+                                if step['outcome']['summary'] == 'failure':
+                                    cases = self.get_test_cases(
+                                        history_id=history['historyId'],
+                                        execution_id=int(execution['executionId']),
+                                        step_id=step['stepId'],
+                                        page_size=int(200)
+                                    )
+                                    # TODO: Decide on which data to capture
+                                    results.append({
+                                        'testCase': [case['testCaseReference'] for case in cases['testCases']],
+                                        'result': [case['status'] for case in cases['testCases']],
+                                        'matrix': execution['testExecutionMatrixId']
+                                    })
+        print(f"{results}")
+
     def get_test_case_results_by_execution_summary(self, execution_outcome_summary: str) -> dict:
-        """Get test case results for executions with a provided outcome summary"""
+        """Get test case results from executions with a provided outcome summary"""
+        history = next(iter([x for y in self.get_histories().values() for x in y]))
+        executions = self.get_executions(
+            history_id=history['historyId'],
+            page_token=None
+        )
         results = []
-        histories = self.get_histories()
-        for history in histories.values():
-            for id in history:
-                executions = self.get_executions(
-                    history_id=id['historyId']
-                )
-                for execution in executions['executions']:
-                    """Filter on complete executions"""
-                    if 'complete' in execution.values():
-                        if execution['outcome']['summary'] == execution_outcome_summary:
-                            steps = self.get_steps(
-                                history_id=id['historyId'],
-                                execution_id=int(execution['executionId']),
-                                page_size=int(200)
-                            )
-                            for k, v in steps.items():
-                                if k == 'steps':
-                                    for step in v:
-                                        if step['outcome']['summary'] == 'failure':
-                                            cases = self.get_test_cases(
-                                                history_id=id['historyId'],
-                                                execution_id=int(execution['executionId']),
-                                                step_id=step['stepId'],
-                                                page_size=int(200)
-                                            )
-                                            for k, v in cases.items():
-                                                if k == 'testCases':
-                                                    for case in v:
-                                                        if case['status'] == 'failed':
-                                                            results.append(
-                                                                case['testCaseReference']
-                                                            )
+
+        for execution in executions['executions']:
+            """Filter on complete executions"""
+            if 'complete' in execution.values():
+                """Executions with flaky tests (of multiple attempts) are treated as successful"""
+                if execution['outcome']['summary'] == execution_outcome_summary:
+                    steps = self.get_steps(
+                        history_id=history['historyId'],
+                        execution_id=int(execution['executionId']),
+                        page_size=int(200)
+                    )
+                    environments = self.get_environments(
+                        history_id=history['historyId'],
+                        execution_id=int(execution['executionId'])
+                    )
+                    for k, v in steps.items():
+                        if k == 'steps':
+                            for step in v:
+                                if step['outcome']['summary'] == 'failure':
+                                    cases = self.get_test_cases(
+                                        history_id=history['historyId'],
+                                        execution_id=int(execution['executionId']),
+                                        step_id=step['stepId'],
+                                        page_size=int(200)
+                                    )
+                                    for k, v in cases.items():
+                                        if k == 'testCases':
+                                            for case in v:
+                                                if case['status'] == 'failed':
+                                                    results.append({
+                                                            'testCase': [case['testCaseReference'] for case in cases['testCases']],
+                                                            'testCaseResult': [case['status'] for case in cases['testCases']],
+                                                            'matrix': execution['testExecutionMatrixId'],
+                                                            'environmentSummary': [environment['environmentResult']['outcome']['summary'] for environment in environments['environments']]
+                                                        }
+                                                    )
         return results
 
     def print_test_results_by_execution_summary(self, execution_outcome_summary: str) -> None:
         results = self.get_test_case_results_by_execution_summary(execution_outcome_summary)
         if results:
             for result in results:
-                print(result)
+                print(f"{result}")
         else:
             print("No results found")
+
+    def display_execution_timestamp(self, execution_outcome_summary: str) -> None:
+        from datetime import datetime
+
+        history = next(iter([x for y in self.get_histories().values() for x in y]))
+        executions = self.get_executions(
+            history_id=history['historyId'],
+            page_token=None
+        )
+        for execution in executions['executions']:
+            """Filter on complete executions"""
+            if 'complete' in execution.values():
+                if execution['outcome']['summary'] == execution_outcome_summary:
+                    for k, v in execution['creationTime'].items():
+                        if k == 'seconds':
+                            dt_obj = datetime.fromtimestamp(
+                                int(v)
+                            )
+                            print(f"{dt_obj} - {execution['testExecutionMatrixId']}")
